71439a8cb3689566a01000182eee9daa58620660
yusuf <yusuffront@gmail.com>
2022-01-31 08:00:46 +0800

    create new controller and UI

---
+++ study_1/resources/views/includes/navbar.blade.php
@@ -0,0 +1,24 @@
+<div class="container ">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Student</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Study</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+</div>
