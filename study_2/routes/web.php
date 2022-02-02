822d1d8d2d2ef91a4734b7cc8ee85be03c4f50f0
yusuf <yusuffront@gmail.com>
2022-02-02 21:01:11 +0800

    learning basic kotlin codes android studio

--- study_2/routes/web.php
+++ study_2/routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', [HomeController::class, 'index']);
 Route::resource('/student', StudentController::class);
 Route::resource('/study', StudyController::class);
 Route::resource('/gallery', GalleryController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
