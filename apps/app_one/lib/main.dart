d051d95699dfb967d74603b64bc615e82a9d0b1f
yusuf <yusuffront@gmail.com>
2022-02-07 02:22:45 +0800

    update more codes

--- apps/app_one/lib/main.dart
+++ apps/app_one/lib/main.dart
@@ -17,19 +17,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello Developer'),
-          backgroundColor: Colors.greenAccent,
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.mail))
-          ],
-        ),
-        body: const Center(
-            child: Image(
-          image: AssetImage('assets/images/ewallet.png'),
-          height: 200,
-        )),
-      ),
+          appBar: AppBar(
+            title: const Text('Hello Developer'),
+            backgroundColor: Colors.deepPurple,
+          ),
+          body: SafeArea(
+            child: Container(
+              color: Colors.deepPurple,
+              margin: EdgeInsets.only(
+                  left: 70.0, top: 10.0, right: 0.0, bottom: 15.0),
+              padding: EdgeInsets.only(
+                  left: 15.0, top: 0.0, right: 0.0, bottom: 15.0),
+              child: Column(
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('assets/images/ewallet.png'),
+                    height: 200,
+                  ),
+                  Text("Hello")
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
