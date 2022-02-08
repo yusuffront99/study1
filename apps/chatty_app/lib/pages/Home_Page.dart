43656acb114ff0b76b7bdd5550f2a0a3f097a039
yusuf <yusuffront@gmail.com>
2022-02-08 19:29:54 +0800

    update more design codes

--- apps/chatty_app/lib/pages/Home_Page.dart
+++ apps/chatty_app/lib/pages/Home_Page.dart
@@ -1,12 +1,46 @@
+import 'package:chatty_app/pages/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Home Page',
-        style: TextStyle(fontSize: 40),
+      backgroundColor: blueColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                'assets/images/profile1.png',
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Jhon Alexander',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: whiteColor,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Travel Freelancer',
+                style: TextStyle(
+                  color: lightblueColor,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ apps/chatty_app/lib/pages/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0XFF1F8DF5);
+Color whiteColor = Color(0XFFFFFFFF);
+Color lightblueColor = Color(0XFF9ED0FF);
+Color blackColor = Color(0XFF2C3A59);
+Color grayColor = Color(0XFF808BA2);
+Color greenColor = Color(0XFF29CB9E);
+
+TextStyle titleTextStyle =
+    TextStyle(color: blackColor, fontWeight: FontWeight.w500, fontSize: 16);
+
+TextStyle subtitleTextStyle =
+    TextStyle(color: greenColor, fontWeight: FontWeight.w300, fontSize: 16);
